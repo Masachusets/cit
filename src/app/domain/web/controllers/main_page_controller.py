45d1775479781e831a84883022db99183f9e6201
Pavlenko_D <Apofeoz@tut.by>
2025-02-06 21:40:04 +0300

    Added main_page_controller and logo.

---
+++ src/app/domain/web/controllers/main_page_controller.py
@@ -0,0 +1,17 @@
+from litestar import Controller, get
+from litestar.response import Template
+
+
+class MainPageController(Controller):
+    path = "/"
+
+    @get(
+        path="",
+        operation_id="GetMainPage",
+        name="main_page:get",
+        summary="Main Page",
+        description="Render the main page of the application",
+    )
+    async def get_main_page(self) -> Template:
+        """Render the main page."""
+        return Template(template_name="pages/main_page.html")
