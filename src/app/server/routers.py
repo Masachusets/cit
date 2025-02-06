2603eb4bf8f643ee5419f129ec7529a70df53afd
Pavlenko_D <Apofeoz@tut.by>
2025-02-06 21:34:08 +0300

    Fixed static.

--- src/app/server/routers.py
+++ src/app/server/routers.py
@@ -1,4 +1,5 @@
 from litestar import Router
+from litestar.static_files import create_static_files_router
 
 from src.app.domain.equipments.controllers import EquipmentController
 from src.app.domain.documents.controller import DocumentController
@@ -7,7 +8,9 @@ from src.app.domain.reference_book.employee.controller import EmployeeController
 from src.app.domain.reference_book.equipment_names.controller import (
     EquipmentNameController,
 )
+
 # from src.app.domain.web.controller import WebIndexController, WebEquipmentController
+from src.app.domain.web.controllers.main_page_controller import MainPageController
 
 
 def create_api_router() -> Router:
@@ -23,17 +26,24 @@ def create_api_router() -> Router:
     )
 
 
-# def create_web_router() -> Router:
-#     return Router(
-#         route_handlers=[
-#             WebIndexController,
-#             WebEquipmentController,
-#         ],
-#     )
+def create_web_router() -> Router:
+    return Router(
+        path="/",
+        route_handlers=[
+            MainPageController,
+            # WebIndexController,
+            # WebEquipmentController,
+        ],
+    )
 
 
 def get_routers() -> list[Router]:
     return [
-        # create_web_router(),
-        create_api_router(),
+        create_web_router(),
+        # create_api_router(),
+        create_static_files_router(
+            path="/static",
+            name="static",
+            directories=["src/app"],
+        ),
     ]
